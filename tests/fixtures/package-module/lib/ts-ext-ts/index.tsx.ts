974935680465e5f42da221ba805653f492cb1455
Hiroki Osame <hiroki.osame@gmail.com>
2022-05-31 13:43:27 -0600

    fix(typescript): allow subextensions

---
+++ tests/fixtures/package-module/lib/ts-ext-ts/index.tsx.ts
@@ -0,0 +1,26 @@
+import fs from 'node:fs';
+
+console.log(
+	'loaded ts-ext-ts/index.tsx.ts',
+	JSON.stringify({
+		nodePrefix: Boolean(fs),
+		hasDynamicImport: Boolean(import('fs')),
+		...(() => {
+			let nameInError;
+			try {
+				nameInError();
+			} catch (error) {
+				return {
+					nameInError: error.message.includes('nameInError'),
+					sourceMap: error.stack.includes(':11:5'),
+				};
+			}
+		})(),
+	}),
+);
+
+function valueNumber(value: number) {
+	return value;
+}
+
+export default valueNumber(1234);
--- tests/specs/typescript/ts.ts
+++ tests/specs/typescript/ts.ts
@@ -47,6 +47,21 @@ export default testSuite(async ({ describe }, node: NodeApis) => {
 			});
 		});
 
+		describe('extensionless with subextension', ({ test }) => {
+			const importPath = './lib/ts-ext-ts/index.tsx';
+			const output = 'loaded ts-ext-ts/index.tsx.ts {"nodePrefix":true,"hasDynamicImport":true,"nameInError":true,"sourceMap":true}';
+
+			test('Load', async () => {
+				const nodeProcess = await node.load(importPath);
+				expect(nodeProcess.stdout).toBe(output);
+			});
+
+			test('Import', async () => {
+				const nodeProcess = await node.import(importPath);
+				expect(nodeProcess.stdout).toBe(`${output}\n{"default":1234}`);
+			});
+		});
+
 		describe('directory', ({ test }) => {
 			const importPath = './lib/ts-ext-ts';
 
