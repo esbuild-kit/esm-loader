2318a38eb58884f6409be58b075e6a2bf3e04903
Hiroki Osame <hiroki.osame@gmail.com>
2022-06-06 23:52:47 -0400

    perf: remove unused code & dependency

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,4 @@
 import path from 'path';
-import { init, parse } from 'es-module-lexer';
 
 export const tsExtensionsPattern = /\.([cm]?ts|[tj]sx)$/;
 
@@ -24,31 +23,3 @@ export type ModuleFormat =
 	| 'wasm';
 
 export type MaybePromise<T> = T | Promise<T>;
-
-let isLexerReady = false;
-
-// eslint-disable-next-line promise/catch-or-return
-init.then(() => {
-	isLexerReady = true;
-});
-
-/**
- * isESM - determined by whether the module
- * uses imports or exports
- */
-const _isEsm = (source: string) => {
-	const [imports, exports] = parse(source);
-
-	return (
-		imports.length > 0
-		|| exports.length > 0
-	);
-};
-
-export function isEsm(source: string) {
-	if (!isLexerReady) {
-		return init.then(() => _isEsm(source));
-	}
-
-	return _isEsm(source);
-}
