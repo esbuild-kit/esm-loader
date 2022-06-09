bbde81223d047302754c61e5e74ba3dcb8511238
Hiroki Osame <hiroki.osame@gmail.com>
2022-06-08 21:47:30 -0400

    wip

--- tests/index.ts
+++ tests/index.ts
@@ -2,17 +2,17 @@ import { describe } from 'manten';
 import { createNode } from './utils/node-with-loader';
 
 const nodeVersions = [
-	'12.20.0', // CJS named export detection added
+	// '12.20.0', // CJS named export detection added
 	'12.22.11',
-	...(
-		process.env.CI
-			? [
-				'14.19.1',
-				'16.13.2',
-				'17.8.0',
-			]
-			: []
-	),
+	// ...(
+	// 	process.env.CI
+	// 		? [
+	// 			'14.19.1',
+	// 			'16.13.2',
+	// 			'17.8.0',
+	// 		]
+	// 		: []
+	// ),
 ];
 
 (async () => {
@@ -21,18 +21,18 @@ const nodeVersions = [
 			describe('Package: module', async ({ runTestSuite }) => {
 				const node = await createNode(nodeVersion, './tests/fixtures/package-module');
 
-				runTestSuite(
-					import('./specs/javascript'),
-					node,
-				);
+				// runTestSuite(
+				// 	import('./specs/javascript'),
+				// 	node,
+				// );
 				runTestSuite(
 					import('./specs/typescript'),
 					node,
 				);
-				runTestSuite(
-					import('./specs/json'),
-					node,
-				);
+				// runTestSuite(
+				// 	import('./specs/json'),
+				// 	node,
+				// );
 			});
 
 			runTestSuite(
--- tests/specs/typescript/tsconfig.ts
+++ tests/specs/typescript/tsconfig.ts
@@ -3,41 +3,43 @@ import type { NodeApis } from '../../utils/node-with-loader';
 
 export default testSuite(async ({ describe }, node: NodeApis) => {
 	describe('tsconfig', ({ test, describe }) => {
-		test('jsxFactory & jsxFragmentFactory', async () => {
-			const nodeProcess = await node.load('./src/tsx.tsx', {
-				cwd: './tsconfig',
-			});
-			expect(nodeProcess.stdout).toBe('div null hello world\nnull null goodbye world');
-		});
+		// test('jsxFactory & jsxFragmentFactory', async () => {
+		// 	const nodeProcess = await node.load('./src/tsx.tsx', {
+		// 		cwd: './tsconfig',
+		// 	});
+		// 	expect(nodeProcess.stdout).toBe('div null hello world\nnull null goodbye world');
+		// });
 
 		describe('paths', ({ test }) => {
 			test('resolves baseUrl', async () => {
 				const nodeProcess = await node.load('./src/base-url.ts', {
 					cwd: './tsconfig',
 				});
-				expect(nodeProcess.stdout).toBe('resolved');
+
+				console.log(nodeProcess);
+				// expect(nodeProcess.stdout).toBe('resolved');
 			});
 
-			test('resolves paths exact match', async () => {
-				const nodeProcess = await node.load('./src/paths-exact-match.ts', {
-					cwd: './tsconfig',
-				});
-				expect(nodeProcess.stdout).toBe('resolved');
-			});
+			// test('resolves paths exact match', async () => {
+			// 	const nodeProcess = await node.load('./src/paths-exact-match.ts', {
+			// 		cwd: './tsconfig',
+			// 	});
+			// 	expect(nodeProcess.stdout).toBe('resolved');
+			// });
 
-			test('resolves paths prefix', async () => {
-				const nodeProcess = await node.load('./src/paths-prefix-match.ts', {
-					cwd: './tsconfig',
-				});
-				expect(nodeProcess.stdout).toBe('resolved');
-			});
+			// test('resolves paths prefix', async () => {
+			// 	const nodeProcess = await node.load('./src/paths-prefix-match.ts', {
+			// 		cwd: './tsconfig',
+			// 	});
+			// 	expect(nodeProcess.stdout).toBe('resolved');
+			// });
 
-			test('resolves paths suffix', async () => {
-				const nodeProcess = await node.load('./src/paths-suffix-match.ts', {
-					cwd: './tsconfig',
-				});
-				expect(nodeProcess.stdout).toBe('resolved');
-			});
+			// test('resolves paths suffix', async () => {
+			// 	const nodeProcess = await node.load('./src/paths-suffix-match.ts', {
+			// 		cwd: './tsconfig',
+			// 	});
+			// 	expect(nodeProcess.stdout).toBe('resolved');
+			// });
 		});
 	});
 });
