bfdde915e1bf3fb7ab5994f6cf88a5018a268885
Hiroki Osame <hiroki.osame@gmail.com>
2022-06-23 03:18:52 -0400

    debug

--- src/loaders.ts
+++ src/loaders.ts
@@ -68,7 +68,7 @@ async function tryDirectory(
 	context: Context,
 	defaultResolve: resolve,
 ) {
-	const appendIndex = specifier.endsWith('/') ? 'index' : `${path.sep}index`;
+	const appendIndex = specifier.endsWith('/') ? 'index' : '/index';
 
 	try {
 		return await tryExtensions(specifier + appendIndex, context, defaultResolve);
