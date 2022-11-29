37f395204798393ea45bcdf77e2ed5ae4ac825af
Hiroki Osame <hiroki.osame@gmail.com>
2022-11-29 13:13:38 +0900

    fix: support data urls

--- tests/specs/data.ts
+++ tests/specs/data.ts
@@ -5,7 +5,7 @@ export default testSuite(async ({ describe }, node: NodeApis) => {
 	describe('data', async ({ test }) => {
 		const importPath = './lib/data/index.js';
 
-		test('Loads data URLs', async () => {
+		test('Loads text/javascript data URLs', async () => {
 			const nodeProcess = await node.load(importPath);
 
 			expect(nodeProcess.exitCode).toBe(0);
