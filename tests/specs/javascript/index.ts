78d1fd3b5bfe7b9c84f6342763fdd32a063507d6
Hiroki Osame <hiroki.osame@gmail.com>
2022-11-30 19:13:13 +0900

    wip

--- tests/specs/javascript/index.ts
+++ tests/specs/javascript/index.ts
@@ -4,7 +4,7 @@ import type { NodeApis } from '../../utils/node-with-loader';
 export default testSuite(async ({ describe }, node: NodeApis) => {
 	describe('JavaScript', ({ runTestSuite }) => {
 		runTestSuite(import('./cjs'), node);
-		// runTestSuite(import('./esm'), node);
-		// runTestSuite(import('./dependencies'), node);
+		runTestSuite(import('./esm'), node);
+		runTestSuite(import('./dependencies'), node);
 	});
 });
