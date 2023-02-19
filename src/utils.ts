15268d86d977567893f426bd72675fa9e26ad5a8
jgoux <hi@jgoux.dev>
2023-02-19 23:51:00 +0100

    oops wrong logic

--- src/utils.ts
+++ src/utils.ts
@@ -36,7 +36,7 @@ function getProjectsMap(tsconfigPath?: string, projectsMap?: Map<string, {
 		return projectsMap;
 	}
 
-	if (!projectsMap.has(tsconfig.path)) {
+	if (projectsMap.has(tsconfig.path)) {
 		return projectsMap;
 	}
 
