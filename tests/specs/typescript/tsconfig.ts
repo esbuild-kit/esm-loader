470ade0d13bdf405b59c15f3ab9c79e947cdedfd
Hiroki Osame <hiroki.osame@gmail.com>
2023-09-11 16:03:13 +0900

    wip

--- tests/specs/typescript/tsconfig.ts
+++ tests/specs/typescript/tsconfig.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+import fs from 'fs/promises';
 import { testSuite, expect } from 'manten';
 import { createFixture } from 'fs-fixture';
 import type { NodeApis } from '../../utils/node-with-loader.js';
@@ -37,6 +39,12 @@ export default testSuite(async ({ describe }, node: NodeApis) => {
 					},
 				});
 
+				await fs.symlink(
+					path.resolve('node_modules'),
+					path.join(fixture.path, 'node_modules'),
+					'dir',
+				);
+
 				onTestFinish(async () => await fixture.rm());
 
 				// Strict mode is not tested because ESM is strict by default
@@ -70,6 +78,12 @@ export default testSuite(async ({ describe }, node: NodeApis) => {
 					}),
 					'src/jsx.jsx': checkJsx,
 				});
+
+				await fs.symlink(
+					path.resolve('node_modules'),
+					path.join(fixture.path, 'node_modules'),
+					'dir',
+				);
 
 				onTestFinish(async () => await fixture.rm());
 
