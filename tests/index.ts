fe1ae1458a8baa8f7041aac7c4454a1c8f6dea76
Hiroki Osame <hiroki.osame@gmail.com>
2023-09-14 08:09:59 +0900

    test: switch default testing to v18 & v20

--- tests/index.ts
+++ tests/index.ts
@@ -2,16 +2,16 @@ import { describe } from 'manten';
 import { createNode } from './utils/node-with-loader.js';
 
 const nodeVersions = [
-	'12.20.0', // CJS named export detection added
-	'12',
+	'18',
+	'20',
 	...(
 		process.env.CI
 			? [
+				'12.20.0', // CJS named export detection added
+				'12',
 				'14',
 				'16',
 				'17',
-				'18',
-				'20',
 			]
 			: []
 	),
