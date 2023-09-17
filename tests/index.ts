e6faea45a9091b281a21b49b2e35985e24632b6c
Hiroki Osame <hiroki.osame@gmail.com>
2023-09-17 15:47:49 +0900

    wip

--- tests/index.ts
+++ tests/index.ts
@@ -2,7 +2,7 @@ import { describe } from 'manten';
 import { createNode } from './utils/node-with-loader.js';
 
 const nodeVersions = [
-	'20',
+	// '20',
 	...(
 		(
 			process.env.CI
@@ -10,11 +10,11 @@ const nodeVersions = [
 		)
 			? [
 				'12.20.0', // CJS named export detection added
-				'12',
-				'14',
-				'16',
-				'17',
-				'18',
+				// '12',
+				// '14',
+				// '16',
+				// '17',
+				// '18',
 			]
 			: []
 	),
@@ -26,36 +26,36 @@ const nodeVersions = [
 			describe('Package: module', async ({ runTestSuite }) => {
 				const node = await createNode(nodeVersion, './tests/fixtures/package-module');
 
-				runTestSuite(
-					import('./specs/javascript/index.js'),
-					node,
-				);
-				runTestSuite(
-					import('./specs/typescript/index.js'),
-					node,
-				);
+				// runTestSuite(
+				// 	import('./specs/javascript/index.js'),
+				// 	node,
+				// );
+				// runTestSuite(
+				// 	import('./specs/typescript/index.js'),
+				// 	node,
+				// );
 				runTestSuite(
 					import('./specs/json.js'),
 					node,
 				);
-				runTestSuite(
-					import('./specs/wasm.js'),
-					node,
-				);
-				runTestSuite(
-					import('./specs/data.js'),
-					node,
-				);
-				runTestSuite(
-					import('./specs/import-map.js'),
-					node,
-				);
+				// runTestSuite(
+				// 	import('./specs/wasm.js'),
+				// 	node,
+				// );
+				// runTestSuite(
+				// 	import('./specs/data.js'),
+				// 	node,
+				// );
+				// runTestSuite(
+				// 	import('./specs/import-map.js'),
+				// 	node,
+				// );
 			});
 
-			runTestSuite(
-				import('./specs/package-cjs.js'),
-				await createNode(nodeVersion, './tests/fixtures/package-commonjs'),
-			);
+			// runTestSuite(
+			// 	import('./specs/package-cjs.js'),
+			// 	await createNode(nodeVersion, './tests/fixtures/package-commonjs'),
+			// );
 		});
 	}
 })();
